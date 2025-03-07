fbd51ca6c739e955bca16a2eb098afdc2adf1c5a
chulkov-alex <chulkov-alex@yandex-team.ru>
2025-03-07 15:13:53 +0300

    1.2.10

--- src/main/kotlin/com/github/mafia/vyasma/polemica/library/model/game/PolemicaGame.kt
+++ src/main/kotlin/com/github/mafia/vyasma/polemica/library/model/game/PolemicaGame.kt
@@ -1,6 +1,8 @@
 package com.github.mafia.vyasma.polemica.library.model.game
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonSetter
+import com.fasterxml.jackson.annotation.Nulls
 import java.time.LocalDateTime
 
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -12,13 +14,19 @@ data class PolemicaGame(
     val scoringType: Int,
     val version: Int,
     val zeroVoting: String?,
-    val tags: List<String>?,
+    @JsonSetter(nulls = Nulls.AS_EMPTY)
+    val tags: List<String>,
+    @JsonSetter(nulls = Nulls.AS_EMPTY)
     val players: List<PolemicaPlayer>,
+    @JsonSetter(nulls = Nulls.AS_EMPTY)
     val checks: List<PolemicaCheck>,
+    @JsonSetter(nulls = Nulls.AS_EMPTY)
     val shots: List<PolemicaShot>,
     val stage: Stage,
+    @JsonSetter(nulls = Nulls.AS_EMPTY)
     val votes: List<PolemicaVote>,
     val comKiller: Position?,
+    @JsonSetter(nulls = Nulls.AS_EMPTY)
     val bonuses: List<PolemicaBonus>,
     val started: LocalDateTime,
     val stop: Stage?,
